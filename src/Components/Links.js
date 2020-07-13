a37c09ec414c65e2ed1c46ad9554540e03280441
syedfawadhashmi5 <59658569+syedfawadhashmi5@users.noreply.github.com>
2020-07-13 05:21:18 -0700

    compele project

--- src/Components/Links.js
+++ src/Components/Links.js
@@ -4,7 +4,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Typography from '@material-ui/core/Typography';
 import Toolbar from '@material-ui/core/Toolbar';
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
 
 
--- src/ProductIndex.js
+++ src/ProductIndex.js
@@ -1,25 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Shoe from './Shoe_Store.json';
-import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
 
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-      '& > *': {
-        margin: theme.spacing(1),
-      },
-    },
-  }));
-
-
 export const ProductIndex = () => {
-    const classes = useStyles();
     return (
         <div>
-                        <h1>WellCome To Shoe Store</h1>
             <div className="productContainer">
                 {Object.keys(Shoe).map(KeyName => {
                     const shoe = Shoe[KeyName];
